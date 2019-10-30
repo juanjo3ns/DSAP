de915102041d1b00343409a7de1ceb75fff6af13
pcaselles <pcaselles22@gmail.com>
2019-10-30 20:53:22 +0100

    spectrogram under developement

--- code/generator.py
+++ code/generator.py
@@ -6,7 +6,7 @@ import pylab
 from IPython import embed
 import matplotlib.pyplot as plt
 import cv2 as cv2
-#import wavio
+import wavio
 
 
 import dataset as dataset
@@ -18,28 +18,16 @@ from torch.utils.data import TensorDataset, DataLoader
 class generator():
     def __init__(self):
         self.data = DataLoader(dataset=dataset.WAV_dataset(mode="train"), batch_size=1, shuffle=False)
-        #self.spectrogram()
-        self.sftp_run(1)
+        
+        self.spectrogram()
+        #self.sftp(0,0)
 
-    def spectrogram(self):
-        for d in self.data:
-            #d[0] = "airport-barcelona-0-0-a.wav"
-            #wav = scipy.io.wavfile.read("/home/data/audio/" + str(d[0]), mmap=False)
-            
-            
-            wav = wavio.read("/home/data/audio/" + str(d[0][0]))
-            rate = wav.rate
-            wav = wav.data[:,1]
-            
-            #Sxx = pf.spectrogram(wav, rate, show=True)
-            #Sxx = pf.spect(wav)
-            #self.save(Sxx, d[0].split(".")[0])
-            pf.spectrogram(wav, rate, name=d[0][0].split(".")[0])
-            
-            print(values)
-        return "done"
-    
-    def sftp_run(self, x, t_size=None, f_size=None, fs=None, framesz=None, show=True):
+
+    def test(self):
+        x, fs = self.sinus_test()
+        wavio.write("/home/data/spectros/test.wav", x, fs, sampwidth=3)
+
+    def sinus_test(self):
         f0 = 500      # Compute the STFT of a 440 Hz sinusoid
         f1 = 3000
         fs = 8000        # sampled at 8 kHz
@@ -49,98 +37,72 @@ class generator():
 
         # Create test signal and STFT.
         t = sc.linspace(0, T, T*fs, endpoint=False)
-        
-        
+
         x = 20*sc.sin(2*sc.pi*f0*t) + sc.sin(2*sc.pi*f1*t) + 1/2*sc.sin(2*sc.pi*f1/2*t)
+
+        return x, fs
+
+    def spectrogram(self):
+        for i, d in enumerate(self.data):
+            #d[0] = "airport-barcelona-0-0-a.wav"
+
+            #Read audio file
+            wav = wavio.read("/home/data/audio/" + str(d[0][0]) + ".wav")
+            rate = wav.rate
+            wav = wav.data[:,0]
+
+            print(np.max(wav))
+            print(wav)
+            
+            Sxx = self.sftp(wav, fs=rate, framesz=0.005, show=True, depth=255)
+            print(Sxx.shape)
+            plt.plot(Sxx[76])
+            plt.show()
+
+            if i == 0:
+                break
+        return "done"
+    
+    def sftp(self, x, fs, t_size=None, f_size=None, framesz=0.005,hop=0.005, depth=255,show=False):
+
+        hop = framesz
+        #Compute the sftp:
         X = np.array(pf.stft(x, fs, framesz, hop, norm=False))
 
+        #Maximum to normalize
         max_Spect = np.max(X)
 
+
+        #Quantitzation of the spectrogram
         f = np.linspace(0,int(fs/2), X.shape[0])
-        yq, mask = pf.quantitzation(f, qLevels=255,Linear=True, Scale=True, show=False)
+        yq, mask = pf.quantitzation(f, qLevels=depth,Linear=True, Scale=True, show=False)
 
-
-        h = np.zeros((X.shape[1], 255))
-
-        
+        Sxx = np.zeros((X.shape[1], depth))
         for i in range(0, X.shape[1]):
             z = X[:, i]
 
             zq = np.ma.masked_where(mask!=1, z)
             zq = np.ma.compressed(zq)
 
-            h[i], _ = pf.quantitzation(zq, qLevels=255, Linear=True, Scale=False, max_value=max_Spect, min_value=0, show=False)
+            Sxx[i], _ = pf.quantitzation(zq, qLevels=depth, Linear=True, Scale=False, max_value=max_Spect, min_value=0, show=False)
 
             #plt.plot(yq, h[i])
             #plt.show()
             #break
 
-        self.save(h, "pol")
-        
-        s = cv2.imread("/home/data/spectros/pol.png", 0)
-
         if show:
         
-            print(h.shape)
+            print(Sxx.shape)
             pylab.figure()
-            
-            pylab.imshow(sc.absolute(s.T), origin='lower', aspect='auto', interpolation='nearest')
+            pylab.imshow(sc.absolute(Sxx.T), origin='lower', aspect='auto', interpolation='nearest')
             pylab.xlabel('Time')
             pylab.ylabel('Frequency')
             pylab.show()
             maxium = np.max(X)
         
-        
-
-
-    def sftp(self):
-        f0 = 500      # Compute the STFT of a 440 Hz sinusoid
-        f1 = 3000
-        fs = 8000        # sampled at 8 kHz
-        T = 12            # lasting 5 seconds
-        framesz = 0.005  # with a frame size of 50 milliseconds
-        hop = framesz     # and hop size of 20 milliseconds.
-
-        # Create test signal and STFT.
-        t = sc.linspace(0, T, T*fs, endpoint=False)
-        
-
-        x = 20*sc.sin(2*sc.pi*f0*t) + sc.sin(2*sc.pi*f1*t) + 1/2*sc.sin(2*sc.pi*f1/2*t)
-        X = np.array(pf.stft(x, fs, framesz, hop, norm=False))
- 
-        
-        z = X[:, 0]
-        f = np.linspace(0,int(fs/2), z.size)
-        yq, mask = pf.quantitzation(f, qLevels=255,Linear=True, Scale=True, show=False)
-        
-
-        zq = np.ma.masked_where(mask!=1, z)
-        zq = np.ma.compressed(zq)
-
-        print(zq.shape)
-        print(np.max(zq))
-        zq, mask = pf.quantitzation(zq, qLevels=255, Linear=True, Scale=False, max_value=1000, show=False)
-        print(zq.shape)
-        print(np.max(zq))
-
-
-        plt.plot(yq, zq)
-        plt.show()
-       
-
-        pylab.figure()
-        
-        pylab.imshow(sc.absolute(X), origin='lower', aspect='auto',
-                    interpolation='nearest')
-        
-        #pylab.imshow(b)
-        pylab.xlabel('Time')
-        pylab.ylabel('Frequency')
-        pylab.show()
-        
-
-        return None
-
+        return Sxx
+        #self.save(h, "pol")
+        #s = cv2.imread("/home/data/spectros/pol.png", 0)
 
     def save(self, img, name):
         path_to_save = "/home/data/spectros/"
--- code/preprocessing_functions.py
+++ code/preprocessing_functions.py
@@ -9,117 +9,6 @@ from numpy import fft
 from IPython import embed
 #from pytfd import helpers as h 
 
-def spectrogram(audio, sf, name):
-    fmin = 100
-    fmax = 20000
-    
-    if sf < fmin:
-        print('Sampling frequency too low.')
-        sys.exit(1)
-
-    # convert to mono
-    #sig = np.mean(audio, axis=1)
-    sig = audio
-    # vertical resolution (frequency)
-    # number of points per segment; more points = better frequency resolution
-    # if equal to sf, then frequency resolution is 1 Hz
-    npts = int(sf/16)
-    npts = int(sf/4)
-
-    # horizontal resolution (time)
-    # fudge factor to keep the number of frequency samples close to 1000
-    # (assuming an image width of about 1000 px)
-    # negative values ought to be fine
-    # this needs to change if image size becomes parametrized
-    winfudge = 1 - ((np.shape(sig)[0] / sf) / 1000)
-
-    print('Calculating FFT...')
-    f, t, Sxx = signal.spectrogram(sig, sf, nperseg=npts, noverlap=int(winfudge * npts))
-
-    # FFT at high resolution makes way too many frequencies
-    # set some lower number of frequencies we would like to keep
-    # final result will be even smaller after pruning
-    nf = 1000
-    # generate an exponential distribution of frequencies
-    # (as opposed to the linear distribution from FFT)
-    b = fmin - 1
-    a = np.log10(fmax - fmin + 1) / (nf - 1)
-    freqs = np.empty(nf, int)
-    for i in range(nf):
-        freqs[i] = np.power(10, a * i) + b
-    # list of frequencies, exponentially distributed:
-    freqs = np.unique(freqs)
-
-    # delete frequencies lower than fmin
-    fnew = f[f >= fmin]
-    cropsize = f.size - fnew.size
-    f = fnew
-    Sxx = np.delete(Sxx, np.s_[0:cropsize], axis=0)
-
-    # delete frequencies higher than fmax
-    fnew = f[f <= fmax]
-    cropsize = f.size - fnew.size
-    f = fnew
-    Sxx = Sxx[:-cropsize, :]
-
-    findex = []
-    # find FFT frequencies closest to calculated exponential frequency distribution
-    for i in range(freqs.size):
-        f_ind = (np.abs(f - freqs[i])).argmin()
-        findex.append(f_ind)
-
-    # keep only frequencies closest to exponential distribution
-    # this is usually a massive cropping of the initial FFT data
-    fnew = []
-    for i in findex:
-        fnew.append(f[i])
-    f = np.asarray(fnew)
-    Sxxnew = Sxx[findex, :]
-    Sxx = Sxxnew
-
-    print('Generating the image...')
-    plt.pcolormesh(t, f, np.log10(Sxx))
-    plt.ylabel('f [Hz]')
-    plt.xlabel('t [sec]')
-    plt.yscale('symlog')
-    #plt.ylim(fmin, fmax)
-
-    # TODO: make this depend on fmin / fmax
-    # right now I'm assuming a range close to 10 - 20000
-    yt = np.arange(10, 100, 10)
-    yt = np.concatenate((yt, 10 * yt, 100 * yt, 1000 * yt))
-    yt = yt[yt <= fmax]
-    yt = yt.tolist()
-    #plt.yticks(yt)
-    print(Sxx.shape)
-    print(Sxx)
-    print(f)
-    #plt.show()
-    path_to_save = "/home/data/spectrogram/"
-    plt.savefig(path_to_save + "plot.png", dpi=300, bbox_inches='tight')
-    img = cv2.imread(path_to_save + "plot.png")
-    img = img[35:1125, 195:1675]
-    path_to_save = "/home/data/spect/"
-    cv2.imwrite(path_to_save + name +".png", img)
-
-def stft_deprecated(x,w, L=None):
-    #Short-time Fourier Transform 
-    #bib: https://gist.github.com/endolith/2784026 
-    # L is the overlap, see http://cnx.org/content/m10570/latest/
-    N = len(x)
-    #T = len(w)
-    if L is None:
-        L = N
-    # Zerro pad the window
-    w = h.zeropad(w, N)
-    X_stft = []
-    points = range(0, N, N//L)
-    for i in points:
-        x_subset = h.subset(x, i, N)
-        fft_subset = fft(x_subset * w)
-        X_stft.append(fft_subset)
-    X_stft = array(X_stft).transpose()
-    return X_stft 
 
 def stft(x, fs=8000, framesz=1, hop=1, window="hamming", norm=True):#framesz and hope in time domain
     framesamp = int(framesz*fs)
@@ -220,7 +109,6 @@ def quantitzation(x, qLevels=20, Linear=True, Scale=False, show=True, max_value=
         print("-"*35)
     
     return yq, mask
-
 
 def fft(x, fs, framesz):
     framesamp = int(framesz*fs)
