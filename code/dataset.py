1c145ac3a290fc3b274761b43b31ee1ee927897c
David Azcona <david.azcona@asu.edu>
2019-11-25 10:24:29 +0000

    Pol changes merge

--- code/dataset.py
+++ code/dataset.py
@@ -10,6 +10,8 @@ from torch.utils.data import Dataset, DataLoader
 from IPython import embed
 import mongodb_api as mongo
 
+import random
+
 class WAV_dataset_task1(Dataset):
 	def __init__(self, paths, mode='train', images=False):
 		self.tags = {"airport":0,
@@ -28,7 +30,7 @@ class WAV_dataset_task1(Dataset):
 		self.images = images
 		self.read_from_database(split=mode)
 
-		print("Total of {} images.".format(len(self.list_names)))
+		#print("Total of {} images.".format(len(self.list_names)))
 
 	def __len__(self):
 		return len(self.list_names)
@@ -57,23 +59,76 @@ class WAV_dataset_task1(Dataset):
 		return img
 
 class WAV_dataset_task5(Dataset):
-	def __init__(self, paths, mode='train', images=False):
+	def __init__(self, paths, mode='train', images=False, mixup={"apply": False, "alfa":0.5, "rate":10}):
 		self.paths = paths
 		self.list_names = [] #cada array de dins correspon al path i al tag
 		self.images = images
 		self.read_from_database(split=mode)
+		self.count = [0,1] #First: For real images, Second: Mixup rate count
+		self.mixup = mixup
 
-		print("Total of {} images.".format(len(self.list_names)))
+		print("Total of {} images.".format(self.__len__()))
 
 	def __len__(self):
-		return len(self.list_names)
+		if self.mixup["apply"]:
+			return len(self.list_names)*(self.mixup["rate"]+1)
+		else:
+			return len(self.list_names)
+
 
 	def __getitem__(self, index):
 
-		img, tag = self.list_names[index]
-		if self.images:
-			img = self.load_image(file_name=img)
-		return img, np.array(tag).astype(int)
+		if self.mixup["apply"]:
+			img, tag = self.apply_mixup(index = index) #With mixup, img and tag is always shuffled
+			tag = np.array(tag).astype(float)
+
+		else:
+			img, tag = self.list_names[index]
+			if self.images:
+				img = self.load_image(file_name=img)
+			tag = np.array(tag).astype(int)
+		
+		img = torch.from_numpy(img).float()
+		return img, tag
+
+	def apply_mixup(self, index):
+		if self.count[1]%(self.mixup["rate"]+1) == 0:
+			self.count[1] = 1
+			img, tag = self.list_names[self.count[0]]
+			tag = np.array(tag).astype(int)
+			if self.images:
+				img = self.load_image(file_name=img) 
+			#print("------------------" + str(self.count[0]))
+			self.count[0] += 1
+
+		else:
+			#print("+")
+			#We took two random index from the real list of images:
+			index1 = random.randint(0, len(self.list_names)-1)
+			index2 = index1
+			while index1==index2:		#Make shure we don't take the same image
+				index2 = random.randint(0, len(self.list_names)-1)
+
+			#We took those names and tag images
+			img1, tag1 = self.list_names[index1]
+			img2, tag2 = self.list_names[index2]
+
+			#We load both images
+			img1 = self.load_image(file_name=img1)
+			img2 = self.load_image(file_name=img2)
+
+			img1 = np.array(img1)
+			img2 = np.array(img2)
+			tag1 = np.array(tag1)
+			tag2 = np.array(tag2)
+
+			#We apply mixup
+			img = (self.mixup["alfa"]*img1+(1-self.mixup["alfa"])*img2)
+			tag = (self.mixup["alfa"]*tag1+(1-self.mixup["alfa"])*tag2)
+
+			self.count[1] +=1
+		
+		return img, np.array(tag)
 
 	def read_from_database(self, split="train"):
 
@@ -82,20 +137,31 @@ class WAV_dataset_task5(Dataset):
 			self.list_names.append([it["file_name"], it["high_labels"]])
 
 
+
 	def load_image(self, file_name):
 		img = utils.load_image(os.path.join(
 			self.paths['spectra'],
 			'spect_task5',
 			file_name.split('.')[0]
 		))
-		img = torch.from_numpy(img).float()
+		#img = torch.from_numpy(img).float()
 
 		return img
 
 if __name__ == '__main__':
-	ds = WAV_dataset_task5(mode='train')
-	for x in ds:
+	paths = {"audio": "/home/data/audio/",
+  			"spectra": "/home/data/spectrogram/",
+  			"weights": "/home/weights/",
+  			"tensorboard": "/home/tensorboard/"}
+
+	ds = WAV_dataset_task5(paths, mode='train', images=True, mixup={"apply": True, "alfa":0.5, "rate":10})
+	dl = DataLoader(dataset=ds, batch_size=1)
+	for i, x in enumerate(dl):
 		img, tag = x
+		print(i+1, end="\r")
 		print(img)
-		print(tag)
-		break
+		#print(tag)
+		if i==10:
+			break
+	print("\n")
+		
